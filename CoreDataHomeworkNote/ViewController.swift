e4185c601d92258b5d2a4f87fc1960ceffdbe71c
Kong vungsovanreach <vungsovanreachkong@gmail.com>
2018-12-08 15:04:14 +0700

    First commit

---
+++ CoreDataHomeworkNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataHomeworkNote
+//
+//  Created by Kong Vungsovanreach on 12/8/18.
+//  Copyright © 2018 Kong Vungsovanreach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
